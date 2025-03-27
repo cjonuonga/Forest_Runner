4e554f5c648a28eb318e090bf8941e745cc026a6
cjonuonga <calebonuonga@yahoo.com>
2025-03-26 17:47:41 -0700

    Move fighter functionality into a class.

--- Fighter.py
+++ Fighter.py
@@ -1,5 +1,84 @@
 import pygame
+import Spreadsheet
 
 class Fighter:
-    def __init__(self, animations):
+    def __init__(self, window):
+
+        self.window = window
+        # Loading sheets for idle and running
+        self.sprite_sheet_image_idle = pygame.image.load("fighter_sheets/Idle.png").convert_alpha()
+        self.sprite_sheet_image_run = pygame.image.load("fighter_sheets/Run.png").convert_alpha()
+
+        # Getting and setting image of character sprite in spritesheets
+        self.sprite_sheet_idle = Spreadsheet.SpriteSheet(self.sprite_sheet_image_idle)
+        self.sprite_sheet_run = Spreadsheet.SpriteSheet(self.sprite_sheet_image_run)
+
+        # Sprite list that holds action spritesheets
+        self.sprite_list = [self.sprite_sheet_idle, self.sprite_sheet_run]
+
+        # Creating animation list
+        self.animation_list = []
+        self.animation_steps = [1,8]
+        self.last_update = pygame.time.get_ticks()
+        self.animation_cooldown = 150
+        self.frame = 0
+        self.BLACK = (0, 0, 0)
+        self.action = 0
+        # Fighter (character position)
+        self.c_x = 0
+        self.c_y = 369
+        self.facing_right = True
+        self.speed = 5
+
+        vel_x = 0
+
+        for i in range(len(self.animation_steps)):
+            self.temp_list = []
+            for j in range(self.animation_steps[i]):
+                self.temp_list.append(self.sprite_list[i].get_image(j, 130, 127, 1.8, self.BLACK))
+
+            self.animation_list.append(self.temp_list)
+    
+    
+    
+    
+    def update(self):
+        self.keys = pygame.key.get_pressed()
+
+        self.action = 0
+
+        if self.keys[pygame.K_d] or self.keys[pygame.K_RIGHT]:
+            self.c_x += self.speed
+            self.action = 1
+            self.facing_right = True
+        elif self.keys[pygame.K_a] or self.keys[pygame.K_LEFT]:
+            self.c_x -= self.speed
+            self.action = 1
+            self.facing_right = False
         
+        
+        self.current_time = pygame.time.get_ticks()
+        if self.current_time - self.last_update >= self.animation_cooldown:
+            self.frame += 1
+            self.last_update = self.current_time
+            if self.frame >= self.animation_cooldown:
+                self.frame = 0
+        
+        if self.frame >= self.animation_steps[self.action]:
+            self.frame = 0
+
+    def draw(self):
+        self.current_frame = self.animation_list[self.action][self.frame]
+
+        if not self.facing_right:
+            self.current_frame = pygame.transform.flip(self.current_frame, True, False).convert_alpha()
+
+        self.window.blit(self.current_frame, (self.c_x, self.c_y))
+    
+    
+    
+
+    
+        
+
+            
--- Forest.py
+++ Forest.py
@@ -3,6 +3,7 @@ import time
 import random
 import sys
 import Spreadsheet
+from Fighter import Fighter
 
 # Initializing Pygame
 pygame.init()
@@ -20,49 +21,8 @@ background_img = pygame.image.load("Forest_pic.JPG")
 # Scaling to fit the screen
 background_img = pygame.transform.scale(background_img, (1200, 600))
 
-# Loading sheets for idle and running
-sprite_sheet_image_idle = pygame.image.load("fighter_sheets/Idle.png").convert_alpha()
-sprite_sheet_image_run = pygame.image.load("fighter_sheets/Run.png").convert_alpha()
 
-# Getting and setting image of character sprite in spritesheets
-sprite_sheet_idle = Spreadsheet.SpriteSheet(sprite_sheet_image_idle)
-sprite_sheet_run = Spreadsheet.SpriteSheet(sprite_sheet_image_run)
-
-# Sprite list that holds action spriteshees
-sprite_list = [sprite_sheet_idle, sprite_sheet_run]
-
-BLACK = (0, 0, 0)
-
-# 130 x 127
-
-# Creating animation list
-animation_list = []
-animation_steps = [1,8]
-action = 0
-last_update = pygame.time.get_ticks()
-animation_cooldown = 150
-frame = 0
-
-
-# Handling controls and movement
-keys = pygame.key.get_pressed()
-vel_x = 0
-
-# Looping through amount of steps within each sprite sheet
-for i in range(len(animation_steps)):
-    temp_list = []
-    for j in range(animation_steps[i]):
-        temp_list.append(sprite_list[i].get_image(j, 130, 127, 1.8, BLACK))
-    
-    animation_list.append(temp_list)
-
-
-c_x = 0
-c_y = 369
-facing_right = True
-speed = 5
-
-
+fighter = Fighter(window)
 running = True
 while running:
     window.blit(background_img, (0, 0))
@@ -71,49 +31,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Storing key pressed state
-    keys = pygame.key.get_pressed()
-
-    action = 0 # Defualt action idle
-
-    # Controlling the Fighter
-    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-        c_x += speed
-        action = 1
-        facing_right = True
-    elif keys[pygame.K_a] or keys[pygame.K_LEFT]:
-        c_x -= speed
-        action = 1
-        facing_right = False
-    
-    
-    # Update animation
-    current_time = pygame.time.get_ticks()
-    if current_time - last_update >= animation_cooldown:
-        frame += 1
-        last_update = current_time
-        if frame >= animation_cooldown:
-            frame = 0
-
-    if frame >= animation_steps[action]:
-        frame = 0
-
-    # Getting current frame from animation list
-    current_frame = animation_list[action][frame]
-
-    # Flip the sprite
-    if not facing_right:
-        current_frame = pygame.transform.flip(current_frame, True, False).convert_alpha()
-    
-    window.blit(current_frame, (c_x, c_y))
+    # Update character/fighter movements and draw in window
+    fighter.update()
+    fighter.draw()
     
 
     # Displaying Character
     pygame.display.update()
     clock.tick(60)
-
-
-pygame.quit()
-sys.exit()
 
 
